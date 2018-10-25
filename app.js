faf3575079f41e21f490361c1ea3525269d8ed50
Ariel Blasi <exorcismo@gmail.com>
2018-10-25 18:42:13 -0300

    test env var

--- app.js
+++ app.js
@@ -8,13 +8,13 @@ async function getLocation() {
 
 //2 get weather data by sending location
 
-async function getWeather(){
+async function getWeather() {
 	const getCoords = await getLocation()
 	const darkSkyKey = '5056977b57b41fa41e56a32728a674e5'
 
 	const fetchWeather = await fetch(
 		`https://api.darksky.net/forecast/${darkSkyKey}/${getCoords.lat},${getCoords.lon}?format=jsonp`,
-		{mode: 'cors'}
+		{ mode: 'cors' }
 	)
 	// const response = await fetchWeather.json()
 	// console.log(response)
@@ -25,4 +25,8 @@ async function getWeather(){
 
 getWeather()
 
-//
+//this test console logs env var of api url, plus the coords
+const getCoords = await getLocation()
+const fullURL = `${process.env.API_URL}/${getCoords.lat},${getCoords.lon}`
+console.log(fullURL)
+
