377a2007f1cdb2ab3c2cdd46e7e0d3bd89b75d04
litesoul <exorcismo@gmail.com>
2018-10-26 14:35:26 -0300

    hardcode coords

--- app.js
+++ app.js
@@ -12,25 +12,18 @@ async function getWeather() {
 	const getCoords = await getLocation()
 	const darkSkyKey = '5056977b57b41fa41e56a32728a674e5'
 
+	// const fetchWeather = await fetch(
+	// 	`https://api.darksky.net/forecast/${darkSkyKey}/${getCoords.lat},${getCoords.lon}`, function (forecast) { })
+
+
 	const fetchWeather = await fetch(
-		`https://api.darksky.net/forecast/${darkSkyKey}/${getCoords.lat},${getCoords.lon}?format=jsonp`,
-		{ mode: 'cors' }
-	)
+		`https://api.darksky.net/forecast/${darkSkyKey}/42.9150,-77.784323`, function (forecast) { })
 	// const response = await fetchWeather.json()
 	// console.log(response)
+	console.log(forecast)
 	console.log(fetchWeather)
 	// return response
 }
 
 getWeather()
-
-//this test console logs env var of api url, plus the coords
-async function test() {
-	// const getCoords = await getLocation()
-	const fullURL = `${process.env.API_URL}/42.91,-77.784`
-	console.log(fullURL)
-}
-
-test()
-
 
