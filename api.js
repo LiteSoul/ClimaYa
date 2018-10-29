f563453532f5cf4fdf9780c61a009dd984976910
Ariel Blasi <exorcismo@gmail.com>
2018-10-29 17:26:46 -0300

    set units to auto and exclude block

--- api.js
+++ api.js
@@ -16,7 +16,7 @@ class Api {
 	async getWeather(coords) {
 		const fetchWeather = await fetch(`
 			https://cors.io/?https://api.darksky.net/forecast/${this.darkSkyKey}/${coords.latitude},${coords.longitude}
-				?lang=es
+				?lang=es&?units=auto?exclude=minutely
 		`)
 		const response = await fetchWeather.json()
 		return response
