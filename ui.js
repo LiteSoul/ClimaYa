8f9003e8c907999c1a84c8ac3f91c3ea9f294d68
litesoul <exorcismo@gmail.com>
2018-10-31 14:46:51 -0300

    more styling

--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ class UI {
 		this.location = document.getElementById('location')
 		this.nowDegree = document.getElementById('now-degree')
 		this.now = document.getElementById('now')
+		this.todayDegree = document.getElementById('today-degree')
 		this.today = document.getElementById('today')
 		this.week = document.getElementById('week')
 		this.icon = document.getElementById('icon')
@@ -19,10 +20,13 @@ class UI {
 			Ahora ${Math.round(weather.currently.temperature)}º (sensación ${Math.round(weather.currently.apparentTemperature)}º)
 		`
 		this.now.textContent = weather.currently.summary
-		this.today.textContent = weather.hourly.summary
 		this.today.innerHTML = `
 		<small>Hoy ${Math.round(weather.currently.temperature)}º</small>${weather.hourly.summary}
 		`
+		this.todayDegree.innerHTML = `
+			Hoy min ${Math.round(weather.daily.data[0].temperatureLow)}º, max ${Math.round(weather.daily.data[0].temperatureHigh)}º
+		`
+		this.today.textContent = weather.hourly.summary
 		this.week.textContent = weather.daily.summary
 		this.icon.src = `
 			https://darksky.net/images/weather-icons/${weather.currently.icon}.png
