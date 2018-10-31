3c10f5f321fbde2c50785568679c5c966193d40c
litesoul <exorcismo@gmail.com>
2018-10-31 11:25:46 -0300

    add icon and spinner

--- ui.js
+++ ui.js
@@ -4,6 +4,9 @@ class UI {
 		this.now = document.getElementById('now')
 		this.today = document.getElementById('today')
 		this.week = document.getElementById('week')
+		this.icon = document.getElementById('icon')
+		this.spinner = document.getElementById('spinner')
+		this.content = document.getElementById('content')
 	}
 
 	display(ip, weather) {
@@ -15,5 +18,11 @@ class UI {
 		this.now.innerText = weather.currently.summary
 		this.today.innerText = weather.hourly.summary
 		this.week.innerText = weather.daily.summary
+		this.icon.src = `https://darksky.net/images/weather-icons/${weather.currently.icon}.png`
+
+		if (weather) {
+			this.spinner.classList.add('hidden')
+			this.content.classList.remove('hidden')
+		}
 	}
 }
